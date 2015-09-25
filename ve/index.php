9dfa136ea0fc93bd4237cdd174601ad1cd9a9f20
knvelasquez <knvelasquez@hotmail.com>
2015-09-25 14:52:10 -0430

    update site

---
+++ ve/index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Kevin Velásquez</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="shortcut icon" href="favicon.ico" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="../assets/css/main.css" />
+		<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="../assets/css/bootstrap-theme.min.css">
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<link rel="stylesheet" href="../assets/font-awesome-4.4.0/css/font-awesome.min.css">	
+	</head>
+	<body>
+	<div style="float:right"><a href="../">English</a></div>
+		<div class="wrapper style11 first" id="top">
+			<article class="container">
+				<div class="row">
+					<div class="col-md-4">
+						<span class="image fit2">
+							<img style="max-height: 300px;" src="../images/thumbs/profile.jpg" style="max-width: 600px; max-height: 200px; padding: 2px; border: 1px solid #666;">
+						</span>
+					</div>
+					<div class="col-md-6">
+						<header>
+							<h2>Hola. Soy <strong><a href="https://kevinvelasquez.herokuapp.com" target="_blank">Kevin Velásquez</a></strong>.</h2>
+						</header>
+						<p style="text-align:justify">Esto es <strong>frame developer</strong>, una agencia de interfaz visual para entornos web, 
+						completamente responsive y de alto rendimiento &#225;gil.
+						</p>
+						<a href="#two" class="button big scrolly">Conozca acerca de lo que hago</a>
+					</div>
+				</div>
+			</article>
+		</div>
+			<section id="two" class="main style2 left dark fullscreen inactive bg001">
+				<div class="content box style2">
+					<h2>Quien soy</h2>
+					<p style="text-align:justify">Soy <a href="https://kevinvelasquez.herokuapp.com" target="_blank" style="color:black">Kevin Velásquez</a>, un desarrollador independiente de Caracas - Venezuela, 
+					experto en muchos lenguajes de programación, 
+					con experiencia en desarrollo web del lado del servidor como del lado del cliente, 
+					con conocimientos sobre patrones de diseño MVC y metodologías orientadas a servicios, 
+					dispuesto a ser entrenado para aumentar mi conocimiento en el área que me desempe&#241;e.</p>
+				</div>
+			</section>
+			<section id="one" class="main style2 right dark fullscreen bg002">
+				<div id="what" class="content box style2">
+					<h2>A que me dedico</h2>
+					<p style="text-align:justify">Mi metodología de desarrollo se basa principalmente en la experiencia adquirida previamente, 
+					las herramientas de trabajo y lenguajes de programación que domino son los siguientes:
+					<a href="https://nodejs.org/en/" target="_blank"><span class="label label-success">node js</span></a> <a href="https://jquery.com/" target="_blank"><span class="label label-warning">jquery</span></a> <a href="http://getbootstrap.com/" target="_blank"><span class="label label-primary" style="background-color:#563d7c">bootstrap</span></a> <a href="https://secure.php.net/" target="_blank"><span class="label label-success">php</span></a> <a href="http://laravel.com/" target="_blank"><span class="label label-danger">laravel</span></a> <a href="http://www.html5rocks.com/es/" target="_blank"><span class="label label-danger">html5</span></a> <a href="http://www.w3schools.com/css/css3_intro.asp" target="_blank"><span class="label label-info">css3</span></a> <a href="https://www.mysql.com/" target="_blank"><span class="label label-success">mysql</span></a> <a href="https://www.visualstudio.com/" target="_blank"><span class="label label-primary" style="background-color:#563d7c">c# Visual Studio .Net</span></a>
+					<a href="https://spring.io/guides/gs/serving-web-content/" target="_blank"><span class="label label-success">Spring MVC</span></a> <a href="https://developers.google.com/chart/interactive/docs/gallery?hl=es" target="_blank"><span class="label label-primary">Chart G</span><span class="label label-danger">o</span><span class="label label-warning">o</span><span class="label label-primary">g</span><span class="label label-success">l</span><span class="label label-danger">e Developers</span></a> <a href="http://www.microsoft.com/en-us/server-cloud/products/sql-server/" target="_blank"><span class="label label-success">MS SQL</span></a>
+					</p>
+				<div id="dvjquery" class="za content box style2" style="width:auto;display:none;position:absolute;background:transparent;left:-120px;top:-80px">
+					<a href="https://jquery.com/" target="_blank" class="button big scrolly label-warning">jquery</a>
+				</div>
+				<div id="dvcss3" class="za content box style2" style="width:auto;display:none;position:absolute;background:transparent;left:-320px;top:-80px">
+					<a href="http://laravel.com/" target="_blank" class="button big scrolly label-danger">laravel</a>
+				</div>
+				<div id="dvbootstrap" class="za content box style2" style="width:auto;display:none;position:absolute;background:transparent;left:-208px;top:0px">
+					<a href="http://getbootstrap.com/" target="_blank" class="button big scrolly label-primary" style="background-color:#563d7c">bootstrap</a>
+				</div>
+				<div id="laravel" class="za content box style2" style="width:auto;display:none;position:absolute;background:transparent;left:-190px;top:80px">
+					<a href="https://nodejs.org/en/" target="_blank" class="button big scrolly label-success">node js</a>
+				</div>
+				<div id="dvhtml5" class="za content box style2" style="height:10px;width:10px;display:none;position:absolute;background:transparent;left:-120px;top:160px">
+					<a href="http://www.html5rocks.com/es/" target="_blank" class="button big scrolly label-danger">html5</a>
+				</div>
+				<div id="dvphp" class="za content box style2" style="width:auto;display:none;position:absolute;background:transparent;left:-320px;top:50px">
+					<a href="http://www.w3schools.com/css/css3_intro.asp" target="_blank" class="button big label-info">css3</a>
+				</div>
+				<div id="dvchart" class="za content box style2" style="width:auto;display:none;position:absolute;background:transparent;left:-520px;top:160px">
+					<a href="https://developers.google.com/chart/interactive/docs/gallery?hl=es" target="_blank" class="button big label-info"><span class="label label-primary">Chart G</span><span class="label label-danger">o</span><span class="label label-warning">o</span><span class="label label-primary">g</span><span class="label label-success">l</span><span class="label label-danger">e Developers</span></a>
+				</div>
+			</div>					
+			</section>
+			<section id="work" class="main style3 primary">
+				<div class="content container">
+					<h2>Experiencias de empleos</h2>
+						<div class="container 75% gallery">
+							<div class="row images">
+								<div class="col-md-6">
+									<div class="image fit from-left">								
+										<div class="panel panel-primary">
+										  <div class="panel-heading">
+											<h3 class="panel-title">Suiche7b, C.A.</h3>
+										  </div>
+										  <div class="panel-body">
+											<div><b>Analista de Sistemas de informacion</b></div>
+											<div>Marzo 2015 - <span class="label label-primary" style="background-color:#563d7c">Actualidad</span></div>
+											<div>Venezuela, Caracas</div>
+											<div align="center" style="height:100px">
+												<a href="http://www.suiche7b.com.ve/" target="_blank">
+													<span class='jobs suiche' style="display:block"></span>	
+												</a>
+											</div>
+										  </div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="image fit from-right">	
+										<div class="panel panel-primary">
+										  <div class="panel-heading">
+											<h3 class="panel-title">Dayco Host, C.A.</h3>
+										  </div>
+										  <div class="panel-body">
+											<div><b>Epecialista I</b></div>
+											<div>Mayo 2014 - Marzo 2015</div>
+											<div>Venezuela, Caracas</div>
+											<div align="center" style="height:100px">
+												<a href="http://www.daycohost.com/" target="_blank">
+													<span class='jobs dayco' style="display:block"></span>
+												</a>
+											</div>
+										  </div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-6">
+									<div class="image fit from-left">
+										<div class="panel panel-primary">
+										  <div class="panel-heading">
+											<h3 class="panel-title">Casa de Cambio Angulo Lopez, C.A.</h3>
+										  </div>
+										  <div class="panel-body">
+											<div><b>Desarrollador Web</b></div>
+											<div>Febrero 2012 - Mayo 2014</div>
+											<div align="center" style="height:100px">
+												<a href="http://www.angulo-lopez.com/" target="_blank">
+													<span class='jobs casa' style="display:block"></span>
+												</a>
+											</div>
+										  </div>
+										</div>
+									</div>
+								</div>								
+								<div class="col-md-6">
+									<div class="image fit from-right">
+										<div class="panel panel-primary">
+										  <div class="panel-heading">
+											<h3 class="panel-title">Banesco Banco Universal, C.A.</h3>
+										  </div>
+										  <div class="panel-body">
+											<div><b>Analista de Sistemas y CRM</b></div>
+											<div>February 2007 - Agosto 2011</div>
+											<div align="center" style="height:100px">
+												<a href="http://www.banesco.com/" target="_blank">
+													<span class='jobs ban' style="display:block"></span>
+												</a>
+											</div>
+										  </div>
+										</div>
+									</div>								
+								</div>	
+						</div>
+					</div>
+				</div>
+			</section>
+			<footer id="footer">
+					<ul class="actions">
+						<li><a href="https://plus.google.com/u/0/110416753239578566298/posts" target="_blank" class="icon fa-google-plus"><span class="label">Gmail</span></a></li>
+						<li><a href="https://instagram.com/knvelasquez" target="_blank" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+					</ul>
+					<ul class="menu">
+						<li>&copy; <a href="https://kevinvelasquez.herokuapp.com" target="_blank">frame developer</a></li><li><a href="https://kevinvelasquez.herokuapp.com" target="_blank">Kevin Velásquez</a></li>
+					</ul>
+			</footer>
+			<script src="../assets/js/jquery-1.11.3.min.js"></script>
+			<script src="../assets/js/jquery-ui.min.js"></script>
+			<script src="../assets/js/bootstrap.min.js"></script>
+			<script src="../assets/js/jquery.scrolly.min.js"></script>
+			<script src="../assets/js/jquery.scrollex.min.js"></script>
+			<script src="../assets/js/skel.min.js"></script>
+			<script src="../assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="../assets/js/main.js"></script>
+	</body>
+</html>
